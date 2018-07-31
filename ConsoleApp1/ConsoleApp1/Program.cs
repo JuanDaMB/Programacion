4eb98bf9a155a0a0521af67b457cd986b0ab0ea8
JuanDaMB <38229434+JuanDaMB@users.noreply.github.com>
2018-07-30 22:35:20 -0500

    Add files via upload

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,18 +10,47 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            #region
-            /*
-             * int minutos;
+            Program pr = new Program();
+            int ejercicio = 0;
+            Console.WriteLine("Hola \nPara mostrar las funciones, presione un número del 1 al 6");
+            ejercicio = int.Parse(Console.ReadLine());
+            
+            switch (ejercicio)
+            {
+                case 1:
+                    pr.Funcion1();
+                    break;
+                case 2:
+                    pr.Funcion2();
+                    break;
+                case 3:
+                    pr.Funcion3();
+                    break;
+                case 4:
+                    pr.Funcion4();
+                    break;
+                case 5:
+                    pr.Funcion5();
+                    break;
+                case 6:
+                    pr.Funcion6();
+                    break;
+                default:
+                    break;
+            }
+            Console.ReadKey();
+        }
+        void Funcion1()
+        {
+            int minutos;
             double costo;
             Console.WriteLine("Ingrese la cantidad de minutos");
             minutos = int.Parse(Console.ReadLine());
             if (minutos <= 3) { costo = 0.50f; Console.WriteLine("Costo: " + costo); }
             else { costo = (0.50f + (0.1f * (minutos - 3))); Console.WriteLine("Costo: " + costo); }
-            */
-            #endregion
-            #region
-            /*
+        }
+        void Funcion2()
+        {
             int numero, factorial = 0;
             Console.WriteLine("Ingrese un número");
             numero = int.Parse(Console.ReadLine());
@@ -30,24 +59,22 @@ namespace ConsoleApp1
                 factorial = factorial + i;
             }
             Console.WriteLine("La suma es: " + factorial);
-            */
-            #endregion
-            #region
-            /*
+        }
+        void Funcion3()
+        {
             int numero, factorial = 0;
             Console.WriteLine("Ingrese un número");
             numero = int.Parse(Console.ReadLine());
             for (int i = 1; i <= numero; i++)
             {if (i % 3 == 0) {factorial = factorial + i; } }
             Console.WriteLine("La suma es: " + factorial);
-            */
-            #endregion
-            #region
-            /*
+        }
+        void Funcion4()
+        {
             int numero,diferencia,usuario, intento = 0;
             Random ran = new Random();
             numero = ran.Next(1, 11);
-            Console.WriteLine(numero + "\n");
+            //Console.WriteLine(numero + "\n");
             Console.WriteLine("Hola \nVamos a jugar un juego \nTendras que adivinar un numero \nAdelante, escribe un número");
             do
             {
@@ -59,13 +86,10 @@ namespace ConsoleApp1
                 else if (diferencia <= 6 && diferencia > 3) { Console.WriteLine("Tibio"); }
                 else if (diferencia <= 9 && diferencia > 6) { Console.WriteLine("Frio"); }
                 else if (diferencia >9) { Console.WriteLine("Heladisimo"); }
-                
-
             } while (diferencia != 0);
-            */
-            #endregion
-            #region
-            /*
+        }
+        void Funcion5()
+        {
             int[] arr = new int[20];
             for (int i = 0; i < arr.Length; i++)
             {
@@ -75,11 +99,10 @@ namespace ConsoleApp1
             Console.WriteLine("Los numeros menores o iguales a 25 son:");
             foreach (int num in arr)
             {if (num <= 25) { Console.WriteLine(num);}}
-            */
-            #endregion
-            #region
+        }
+        void Funcion6()
+        {
             string[,] matrix = new string[20, 20];
-            
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
                 for (int j = matrix.GetLength(1) - 1; j >= 0; j--)
@@ -91,21 +114,13 @@ namespace ConsoleApp1
                     if(i == j) {matrix[i, j] = "X";}
                     if (matrix[i,j] != "X")   { matrix[i, j] = "O"; }
                 }
-                
             }
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
                 for (int j = 0; j < matrix.GetLength(1); j++)
-                {
-                    Console.Write(string.Format("{0} ", matrix[i, j]));
-                }
+                {Console.Write(string.Format("{0} ", matrix[i, j])); }
                 Console.Write("\n");
             }
-            
-
-
-            #endregion
-            Console.ReadKey();
         }
     }
 }
